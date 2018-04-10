f97d857e7a9b92da8af2642bb1d6770266c314e5
Sergii Katruk <katruksergyi@gmail.com>
2018-04-10 22:07:44 +0300

    Controllers for publication requests and rental objects are add to WebApi

---
+++ WebApi/WebApi/Controllers/PublicationRequestController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Models.Rent;
+using Repository;
+
+namespace WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/PublicationRequest")]
+    public class PublicationRequestController : Controller
+    {
+        private readonly IRepositoryProvider repository = RepositoryProvider.RepositoryProvider.GetRepository();
+        // GET: api/PublicationRequest
+        [HttpGet]
+        public IEnumerable<PublicationRequest> Get()
+        {
+            return this.repository.PublicationRequestRepository.Get(0, 100);
+        }
+
+        // GET: api/PublicationRequest/5
+        [HttpGet("{id}", Name = "Get")]
+        public PublicationRequest Get(int id)
+        {
+            return this.repository.PublicationRequestRepository.GetById(id);
+        }
+        
+        // POST: api/PublicationRequest
+        [HttpPost]
+        public void Post([FromBody]PublicationRequest value)
+        {
+            this.repository.PublicationRequestRepository.Add(value);
+        }
+        
+        // PUT: api/PublicationRequest/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]PublicationRequest value)
+        {
+            this.repository.PublicationRequestRepository.Save(value);
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.repository.PublicationRequestRepository.Remove(id);
+        }
+    }
+}
---
+++ WebApi/WebApi/Controllers/RentalObjectController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Models.Rent;
+using Repository;
+
+namespace WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/RentalObject")]
+    public class RentalObjectController : Controller
+    {
+        private readonly IRepositoryProvider repository = RepositoryProvider.RepositoryProvider.GetRepository();
+        // GET: api/RentalObject
+        [HttpGet]
+        public IEnumerable<RentalObject> Get()
+        {
+            return this.repository.RentalObjectRepositroy.Get(0, 100);
+        }
+
+        // GET: api/RentalObject/5
+        [HttpGet("{id}")]
+        public RentalObject Get(int id)
+        {
+            return this.repository.RentalObjectRepositroy.GetById(id);
+        }
+        
+        // POST: api/RentalObject
+        [HttpPost]
+        public void Post([FromBody]RentalObject value)
+        {
+            this.repository.RentalObjectRepositroy.Add(value);
+        }
+        
+        // PUT: api/RentalObject/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]RentalObject value)
+        {
+            this.repository.RentalObjectRepositroy.Save(value);
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.repository.RentalObjectRepositroy.Remove(id);
+        }
+    }
+}
